48df4714854bc0686f43c7fb2592fe4947d4bbb4
Suzi <suzil@indeed.com>
2024-07-17 14:12:40 +0900

    Adding the breadcrumb navigation bar

--- src/Layout/CreateDeck.js
+++ src/Layout/CreateDeck.js
@@ -4,17 +4,30 @@ import { Link } from 'react-router-dom';
 function CreateDeck() {
     return (
         <div>
-            <h1>Create Deck</h1>
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="/">
+                    <i class="oi oi-home"></i> Home
+                    </a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Create Deck</li>
+                </ol>
+            </nav>
 
+            <h1>Create Deck</h1>
             <label for="deck-name">Name</label>
-            <br/>
+            <br />
             <input class="form-control" name="deck-name" type="text" placeholder="Deck Name" />
 
-            <br/>
+            <br />
 
             <label for="deck-description">Description</label>
-            <br/>
+            <br />
             <textarea class="form-control" rows="5" name="deck-description" type="text" placeholder="Brief description of the deck" />
+            <br />
+
+            <button type="cancel" class="btn btn-secondary">Cancel</button>
+            {" "}
+            <button type="submit" class="btn btn-primary">Submit</button>
         </div>
     )
 }
