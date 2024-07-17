0e9629ad6d12c9d1b4249a212f0cd6e4a5efe11f
Suzi <suzil@indeed.com>
2024-07-17 13:55:52 +0900

    building the create deck ui

--- src/Layout/CreateDeck.js
+++ src/Layout/CreateDeck.js
@@ -5,6 +5,16 @@ function CreateDeck() {
     return (
         <div>
             <h1>Create Deck</h1>
+
+            <label for="deck-name">Name</label>
+            <br/>
+            <input class="form-control" name="deck-name" type="text" placeholder="Deck Name" />
+
+            <br/>
+
+            <label for="deck-description">Description</label>
+            <br/>
+            <textarea class="form-control" rows="5" name="deck-description" type="text" placeholder="Brief description of the deck" />
         </div>
     )
 }
