584d28159d13000a42c5d812448095ae24403c6c
Suzi <suzil@indeed.com>
2024-07-18 10:10:38 +0900

    Study card implementation

--- src/Layout/StudyDeck.js
+++ src/Layout/StudyDeck.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 
 function StudyDeck({decks}) {
     const [cardIndex,setCardIndex] = useState(0);
+    const [flipped,setFlipped] = useState(false);
+    const [clicked, setClicked] = useState(false);
 
     const {deckId} = useParams();
     const deck = decks.find(deck => deck.id == deckId);
@@ -11,14 +13,28 @@ function StudyDeck({decks}) {
     }
     {/* TODO: update the breadcrumb navigation bar */}
 
+    {/*instead of: 
+        onClick={()=>setFlipped(!flipped)}
+        a function that also makes the next button visible
+        */}
+    const handleClick = () => {
+        setFlipped(!flipped);
+        if (!clicked) {
+          console.log('This will only happen on the first click');
+          setClicked(true); //this will only happen on the first click, and will make the next button show
+        }
+      };
+
     return (
         <div>
             <h3>Study: {deck.name}</h3>
             <div className="card">
                 <div className="card-body" >
                     <h4>Card {cardIndex+1} of {deck.cards.length}</h4>
-                    <p>{deck.cards[cardIndex].front}</p>
-                    <button type="button" class="btn btn-secondary">Flip</button>
+                    <p>{flipped ? deck.cards[cardIndex].back : deck.cards[cardIndex].front}</p>
+                    <button type="button" class="btn btn-secondary" onClick={handleClick}>Flip</button>
+                    {' '}
+                    {clicked && <button type="button" class="btn btn-primary" onClick={()=>setCardIndex(cardIndex+1)}>Next</button>}
                 </div>
             </div>
         </div>
